b84b097724ac0ed33fac4d6074bf4f182fa5d2b8
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2021-05-01 18:41:15 +0900

    first-commit

---
+++ database/migrations/2021_04_27_215519_create_shops_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateShopsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('shops', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('img');
+            $table->id('area_id');
+            $table->id('genre_id');
+            $table->string('content');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('shops');
+    }
+}
