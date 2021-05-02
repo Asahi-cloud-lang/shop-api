2147088148f0ed99f17c5eb750747770f1fe003a
Asahi-cloud-lang <hiroto_asahi@yahoo.co.jp>
2021-05-02 09:09:32 +0900

    Cotroller-Route-Create

--- routes/api.php
+++ routes/api.php
@@ -1,19 +1,21 @@
 <?php
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShopsController;
+use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\UsersController;
+use App\Http\Controllers\LikesController;
+use App\Http\Controllers\ReservationsController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+Route::apiResource('/shops', ShopsController::class);
+Route::post('/register', [RegisterController::class, 'post']);
+Route::post('/login', [LoginController::class, 'post']);
+Route::post('/logout', [LogoutController::class, 'post']);
+Route::get('/users', [UsersController::class, 'get']);
+Route::post('/likes', [LikesController::class, 'post']);
+Route::delete('/likes', [LikesController::class, 'delete']);
+Route::get('/reservations', [ReservationsController::class, 'get']);
+Route::post('/reservations', [ReservationsController::class, 'post']);
+Route::delete('/reservations', [ReservationsController::class, 'delete']);
